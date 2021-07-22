8f66e2f444913d1ce3aef1755c9ac3221b7be8d2
rina-watanabe <rina-watanabe@cybozu.co.jp>
2021-07-22 14:58:35 +0900

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,13 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+    {
       resolve: `gatsby-plugin-styled-components`,
       options: {
         // Add any options here
