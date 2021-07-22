af92ac13fb3de5e5901df70984e07188bf33ab78
rina-watanabe <rina-watanabe@cybozu.co.jp>
2021-07-22 14:46:47 +0900

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,17 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "My Gatsby Site",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: [
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+        displayName: false
+      },
+    },
+  ],
+
 };
---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+// Step 1: Import your component
+import * as React from 'react'
+// import { Link } from 'gatsby'
+import Layout from '../components/layout'
+
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>Aputページでござる！</p>
+    </Layout>
+  )
+}
+
+// Step 3: Export your component
+export default AboutPage
